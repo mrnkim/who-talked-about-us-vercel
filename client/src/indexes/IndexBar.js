4e2d465b1d6705e8d3e42318fb3e25a2ddb20da7
MKim <mrn.kim07@gmail.com>
2024-05-17 16:14:33 +0900

    Remove delete index function and icon

--- client/src/indexes/IndexBar.js
+++ client/src/indexes/IndexBar.js
@@ -1,7 +1,3 @@
-import { useState } from "react";
-import ErrorFallback from "../common/ErrorFallback";
-import { Modal, Button } from "react-bootstrap";
-import closeIcon from "../svg/Close.svg";
 import "./IndexBar.css";
 
 /** Index bar that shows index name and number of total videos
@@ -11,93 +7,19 @@ import "./IndexBar.css";
  */
 
 export function IndexBar({ index }) {
-  const [showDeleteButton, setShowDeleteButton] = useState(false);
-  const [showIndexId, setShowIndexId] = useState(false);
-  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
-  const [deleteError, setDeleteError] = useState(null);
-
-  const showDeleteConfirmationMessage = () => {
-    setShowDeleteConfirmation(true);
-  };
-
-  const hideDeleteConfirmationMessage = () => {
-    setShowDeleteConfirmation(false);
-  };
-
-  async function deleteIndex() {
-    try {
-      await deleteIndexMutation.mutateAsync(index._id);
-      hideDeleteConfirmationMessage();
-    } catch (error) {
-      setDeleteError(error);
-    }
-  }
-
   return (
     <>
-      {!deleteError && (
-        <div
-          onMouseEnter={() => {
-            setShowDeleteButton(true);
-            setShowIndexId(true);
-          }}
-          onMouseLeave={() => {
-            setShowDeleteButton(false);
-            setShowIndexId(false);
-          }}
-          className="default-index"
-        >
-          {/* Index Bar */}
+      {
+        <div className="default-index">
           <div className="indexBar">
-            <i className="bi bi-folder"></i>
-            <span className="indexName">{index.index_name}</span>
-            <span>({index && index.video_count} videos)</span>
-            {showIndexId && <div className="indexId">Id: {index._id}</div>}
+            <div>
+              <i className="bi bi-folder"></i>
+              <span className="indexName">{index.index_name}</span>
+              <span>({index && index.video_count} videos)</span>
+            </div>
           </div>
-
-          {/* Delete Index Button */}
-          <div className="deleteButtonWrapper">
-            {showDeleteButton && (
-              <>
-                <button
-                  className="deleteButton"
-                  onClick={showDeleteConfirmationMessage}
-                >
-                  {closeIcon && (
-                    <img src={closeIcon} alt="Icon" className="icon" />
-                  )}
-                </button>
-              </>
-            )}
-          </div>
-
-          {/* Delete Index Confirmation Message */}
-          {showDeleteConfirmation && (
-            <Modal
-              show={showDeleteConfirmation}
-              onHide={hideDeleteConfirmationMessage}
-              backdrop="static"
-              keyboard={false}
-            >
-              <Modal.Body>
-                Are you sure you want to delete this index?
-              </Modal.Body>
-              <Modal.Footer>
-                <Button variant="danger" onClick={deleteIndex}>
-                  Delete
-                </Button>
-                <Button
-                  variant="secondary"
-                  onClick={hideDeleteConfirmationMessage}
-                >
-                  Cancel
-                </Button>
-              </Modal.Footer>
-            </Modal>
-          )}
         </div>
-      )}
-      {deleteError && <ErrorFallback error={deleteError} />}
+      }
     </>
   );
 }
