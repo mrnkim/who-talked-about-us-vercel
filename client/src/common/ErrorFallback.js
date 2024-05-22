5243de3ff2f586063aa7f26c5e7b24d12b76cb4a
MKim <mrn.kim07@gmail.com>
2024-05-22 16:42:01 +0900

    Add Back to Home button in ErrorFallback

--- client/src/common/ErrorFallback.js
+++ client/src/common/ErrorFallback.js
@@ -4,6 +4,10 @@ import "./ErrorFallback.css";
 
 /** Component to show when there is an error */
 function ErrorFallback({ error }) {
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
   return (
     <div role="alert">
       <p>Something went wrong</p>
@@ -11,6 +15,9 @@ function ErrorFallback({ error }) {
         <img src={WarningIcon} alt="WarningIcon" className="icon"></img>
         <div className="warningMessage">{error?.message || error.error}</div>
       </div>
+      <button className="resetButton" onClick={handleRefresh}>
+        Back to Home
+      </button>
     </div>
   );
 }
